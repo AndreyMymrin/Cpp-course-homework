940a005a42cf4aa2a70340e02e41ca0bff6023f1
AndreyMymrin <113207976+AndreyMymrin@users.noreply.github.com>
2023-02-17 11:23:21 +0300

    time counter

---
+++ SEM2/Time_counter/Time_counter/Time_counter.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <chrono>
+
+using namespace std::chrono;
+int function(int n);
+
+steady_clock::duration Time_counter(int f(int), int n) {
+
+    steady_clock::time_point t_start = steady_clock::now();
+    f(n);
+    steady_clock::time_point t_end = steady_clock::now();
+    return t_end - t_start;
+}
+
+int function(int n) {
+    int a = 0;
+    for (int i = 0; i < n; i++) {
+        a += i;
+    }
+    //std::cout << a << "\n"; // release works!
+    return a;
+}
+
+int main()
+{
+    steady_clock::duration T = Time_counter(function, 1000000);
+    
+    std::cout << duration_cast<microseconds>(T).count()<<" microseconds";
+
+    //std::cout << T.count();
+}
