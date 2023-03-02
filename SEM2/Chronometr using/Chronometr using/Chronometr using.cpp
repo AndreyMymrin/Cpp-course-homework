c62233332ab1996e128acd0aa04eeef7005b14df
AndreyMymrin <113207976+AndreyMymrin@users.noreply.github.com>
2023-03-02 16:04:55 +0300

    вектор нехватка памяти

---
+++ SEM2/Chronometr using/Chronometr using/Chronometr using.cpp
@@ -0,0 +1,33 @@
+﻿#include <chrono>
+#include <iostream>
+
+#include <vector>
+#include <array>
+#include <deque>
+#include <list>
+#include <forward_list>
+
+using namespace std::chrono;
+template <typename F, typename... Args>
+steady_clock::duration Time_counter(F Function, Args... args)
+{
+    steady_clock::time_point t_start = steady_clock::now();
+    Function(args...);
+    steady_clock::time_point t_end = steady_clock::now();
+    return t_end - t_start;
+}
+int main()
+{
+    srand(time(0));
+    int a[] = { 10, 1000, 100000 };
+    for (int i : a) {
+        std::vector<int> data;
+        for (int j = 0; j < i; j++) {
+            data.push_back(rand() % 100);
+        }
+        std::cout << i << ' ' << duration_cast<microseconds>(Time_counter(std::vector<int>::push_back, 5)).count() << " microseconds\n";
+}
+
+
+//Используя разработанный хронометр измерьте время рассмотренных на семинаре операций для 5 типов контейнеров,
+//при заполненности 10, 1000, 100000 различных элементов.Составе таблицу.
---
+++ SEM2/vector overdata/vector data/vector data/vector data.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+int main()
+{
+    std::vector<int> v = { 1 };
+    std::cout << v.size() << ' ' << v.capacity() << '\n';
+    for (int i = 0; i < 10000000; i++) {
+        int prev_capacity = v.capacity();
+        v.push_back(i);
+        if (v.capacity() != prev_capacity) std::cout << '\n' << v.size() << ' ' << v.capacity() << ' ' << (float)v.capacity() / prev_capacity << '\n';
+    }
+
+}
+
+//Определите, во сколько раз расширяется пространство выделяемое под std::vector при нехватке памяти.
+
+/*
+Ответ: увеличивается в 1.5 раза
+*/
